a1447a047ca60c411151d866b8458553f3706f53
Seongho Park <bluetosh7@gmail.com>
2018-12-03 18:18:19 +0900

    feat: grid search

--- bin/runner.py
+++ bin/runner.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
+from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.metrics import classification_report
 from plots import draw_corr_heatmap
 import seaborn as sns
 import xgboost as xgb
@@ -11,8 +13,8 @@ import pickle
 from logger import Logger
 
 # Varaibles
-train_rate = .85
-model_name = 'xgboost-test2'
+train_rate = .8
+model_name = 'xgboost-normalize-test3'
 
 np.random.seed(0)
 
@@ -20,6 +22,15 @@ names = ['contract date', 'latitude', 'longtitude', 'altitude', '1st region id',
          'apartment id', 'floor', 'angle', 'area', 'parking lot limit', 'parking lot area', 'parking lot external',
          'management fee', 'households', 'age of residents', 'builder id', 'completion date', 'built year',
          'schools', 'bus stations', 'subway stations', 'price']
+
+tuned_parameters = {
+  'n_estimators': [100, 200, 400, 800],
+  'learning_rate': [0.001, 0.002, 0.004, 0.008, 0.01, 0.02, 0.04, 0.08, 0.1, 0.2, 0.4, 0.8],
+  'gamma': [0, 0.5, 1, 1.5, 2, 5],
+  'subsample': [0.5, 0.66, 0.75],
+  'colsample_bytree': [0.6, 0.8, 1],
+  'max_depth': [3, 4, 5, 6, 7, 8]
+}
 
 data = pd.read_csv('../data/data_train.csv',
                    names=names)
@@ -56,7 +67,7 @@ def get_accuracy(y_pred, y_test):
   return 1 - (_sum / length)
   
 # Main
-logger = Logger('xgboost')
+logger = Logger('xgboost-grid-search')
 
 X, y = get_X_y(data)
 X_names = list(X)
@@ -77,10 +88,31 @@ def train():
   model.fit(X_train, y_train)
   pickle.dump(model, open(f"../models/{model_name}.dat", "wb"))
 
+def print_cross_val():
+  model = xgb.XGBRegressor()
+
+  def acc_scorer(model, X, y):
+    y_pred = model.predict(X)
+    return get_accuracy(y_pred, y.iloc)
+
+  # print(cross_val_score(model, X, y, scoring=acc_scorer, cv=5, n_jobs=-1))
+  model = GridSearchCV(model, tuned_parameters, cv=5,
+                     scoring=acc_scorer)
+  model.fit(X, y)
+
+  logger.write("Best params:")
+  logger.write(model.best_params_)
+  logger.write("Grid scores:")
+  means = model.cv_results_['mean_test_score']
+  stds = model.cv_results_['std_test_score']
+  for mean, std, params in zip(means, stds, model.cv_results_['params']):
+      logger.write("%0.3f (+/-%0.03f) for %r"
+            % (mean, std * 2, params))
+
 def test():
   model = pickle.load(open(f"../models/{model_name}.dat", "rb"))
   y_pred = model.predict(X_test)
-  # print(get_accuracy(y_pred, y_test.iloc))
+  print(get_accuracy(y_pred, y_test.iloc))
 
 def print_importances(names):
   model = pickle.load(open(f"../models/{model_name}.dat", "rb"))
@@ -94,5 +126,6 @@ def print_importances(names):
 
 # train()
 # test()
-print_importances(names)
+# print_importances(names)
 # print_importances(X_names)
+print_cross_val()
